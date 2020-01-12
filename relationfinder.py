7b58a09d17434fb77cfa5fd1c78cda2ffdd74f2c
Kishore <kishorekunisetty123@gmail.com>
2020-01-12 21:55:12 +0530

    takes only integers and returns an algebraic relation among them
    if do you have any better version or approach please feel free to make a pull request and it will be appreciated.

---
+++ relationfinder.py
@@ -0,0 +1,138 @@
+if __name__=='__main__':
+    print('enter 5 numbers so that you can find arthamatic relations amoung them :')
+    n1=int(input(''))
+    n2=int(input(''))#1:+ 2:- 3:* 4:/
+    n3=int(input(''))
+    n4=int(input(''))
+    n5=int(input(''))
+    result=0
+    if n1+n2+n3+n4==n5:
+        print(n1,'+', n2,'+', n3,'+', n4,'=', n5)
+    elif n1+n2+n3-n4==n5:
+        print(n1,'+', n2,'+', n3,'-', n4,'=', n5)
+    elif n1+n2+n3*n4==n5:
+        print(n1,'+', n2,'+', n3,'*', n4,'=', n5)
+    elif n1+n2+n3/n4==n5:
+        print(n1,'+', n2,'+', n3,'/', n4,'=', n5)
+    elif n1+n2-n3+n4==n5:
+        print(n1,'+', n2,'-', n3,'+', n4,'=', n5)
+    elif n1+n2-n3-n4==n5:
+        print(n1,'+', n2,'-', n3,'-', n4,'=', n5)
+    elif n1+n2-n3*n4==n5:
+        print(n1,'+', n2,'-', n3,'*', n4,'=', n5)
+    elif n1+n2-n3/n4==n5:
+        print(n1,'+', n2,'-', n3,'/', n4,'=', n5)
+    elif n1+n2*n3+n4==n5:
+        print(n1,'+', n2,'*', n3,'+', n4,'=', n5)
+    elif n1+n2*n3-n4==n5:
+        print(n1,'+', n2,'*', n3,'-', n4,'=', n5)
+    elif n1+n2*n3*n4==n5:
+        print(n1,'+', n2,'*', n3,'*', n4,'=', n5)
+    elif n1+n2*n3/n4==n5:
+        print(n1,'+', n2,'*', n3,'/', n4,'=', n5)
+    elif n1+n2/n3+n4==n5:
+        print(n1,'+', n2,'/', n3,'+', n4,'=', n5)
+    elif n1+n2/n3-n4==n5:
+        print(n1,'+', n2,'/', n3,'-', n4,'=', n5)
+    elif n1+n2/n3*n4==n5:
+        print(n1,'+', n2,'/', n3,'*', n4,'=', n5)
+    elif n1+n2/n3/n4==n5:
+        print(n1,'+', n2,'/', n3,'/', n4,'=', n5)
+    elif n1-n2+n3+n4==n5:
+        print(n1, '-', n2,'+', n3,'+', n4,'=', n5)
+    elif n1-n2+n3-n4==n5:
+        print(n1,'-', n2,'+', n3,'-', n4,'=', n5)
+    elif n1-n2+n3*n4==n5:
+        print(n1,'-', n2,'+', n3,'*', n4,'=', n5)
+    elif n1-n2+n3/n4==n5:
+        print(n1,'-', n2,'+', n3,'/', n4,'=', n5)
+    elif n1-n2-n3+n4==n5:
+        print(n1,'-', n2,'-', n3,'+', n4,'=', n5)
+    elif n1-n2-n3-n4==n5:
+        print(n1,'-', n2,'-', n3,'-', n4,'=', n5)
+    elif n1-n2-n3*n4==n5:
+        print(n1,'-', n2,'-', n3,'*', n4,'=', n5)
+    elif n1-n2-n3/n4==n5:
+        print(n1,'-', n2,'-', n3,'/', n4,'=', n5)
+    elif n1-n2*n3+n4==n5:
+        print(n1,'-', n2,'*', n3,'+', n4,'=', n5)
+    elif n1-n2*n3-n4==n5:
+        print(n1,'-', n2,'*', n3,'-', n4,'=', n5)
+    elif n1-n2*n3*n4==n5:
+        print(n1,'-', n2,'*', n3,'*', n4,'=', n5)
+    elif n1-n2*n3/n4==n5:
+        print(n1,'-', n2,'*', n3,'/', n4,'=', n5)
+    elif n1-n2/n3+n4==n5:
+        print(n1,'-', n2,'/', n3,'+', n4,'=', n5)
+    elif n1-n2/n3-n4==n5:
+        print(n1,'-', n2,'/', n3,'-', n4,'=', n5)
+    elif n1-n2/n3*n4==n5:
+        print(n1,'-', n2,'/', n3,'*', n4,'=', n5)
+    elif n1-n2/n3/n4==n5:
+        print(n1,'-', n2,'/', n3,'/', n4,'=', n5)
+    elif n1*n2+n3+n4==n5:
+        print(n1, '*', n2,'+', n3,'+', n4,'=', n5)
+    elif n1*n2+n3-n4==n5:
+        print(n1,'*', n2,'+', n3,'-', n4,'=', n5)
+    elif n1*n2+n3*n4==n5:
+        print(n1,'*', n2,'+', n3,'*', n4,'=', n5)
+    elif n1*n2+n3/n4==n5:
+        print(n1,'*', n2,'+', n3,'/', n4,'=', n5)
+    elif n1*n2-n3+n4==n5:
+        print(n1,'*', n2,'-', n3,'+', n4,'=', n5)
+    elif n1*n2-n3-n4==n5:
+        print(n1,'*', n2,'-', n3,'-', n4,'=', n5)
+    elif n1*n2-n3*n4==n5:
+        print(n1,'*', n2,'-', n3,'*', n4,'=', n5)
+    elif n1*n2-n3/n4==n5:
+        print(n1,'*', n2,'-', n3,'/', n4,'=', n5)
+    elif n1*n2*n3+n4==n5:
+        print(n1,'*', n2,'*', n3,'+', n4,'=', n5)
+    elif n1*n2*n3-n4==n5:
+        print(n1,'*', n2,'*', n3,'-', n4,'=', n5)
+    elif n1*n2*n3*n4==n5:
+        print(n1,'*', n2,'*', n3,'*', n4,'=', n5)
+    elif n1*n2*n3/n4==n5:
+        print(n1,'*', n2,'*', n3,'/', n4,'=', n5)
+    elif n1*n2/n3+n4==n5:
+        print(n1,'*', n2,'/', n3,'+', n4,'=', n5)
+    elif n1*n2/n3-n4==n5:
+        print(n1,'*', n2,'/', n3,'-', n4,'=', n5)
+    elif n1*n2/n3*n4==n5:
+        print(n1,'*', n2,'/', n3,'*', n4,'=', n5)
+    elif n1*n2/n3/n4==n5:
+        print(n1,'*', n2,'/', n3,'/', n4,'=', n5)
+    elif n1/n2+n3+n4==n5:
+        print(n1, '/', n2,'+', n3,'+', n4,'=', n5)
+    elif n1/n2+n3-n4==n5:
+        print(n1,'/', n2,'+', n3,'-', n4,'=', n5)
+    elif n1/n2+n3*n4==n5:
+        print(n1,'/', n2,'+', n3,'*', n4,'=', n5)
+    elif n1/n2+n3/n4==n5:
+        print(n1,'/', n2,'+', n3,'/', n4,'=', n5)
+    elif n1/n2-n3+n4==n5:
+        print(n1,'/', n2,'-', n3,'+', n4,'=', n5)
+    elif n1/n2-n3-n4==n5:
+        print(n1,'/', n2,'-', n3,'-', n4,'=', n5)
+    elif n1/n2-n3*n4==n5:
+        print(n1,'/', n2,'-', n3,'*', n4,'=', n5)
+    elif n1/n2-n3/n4==n5:
+        print(n1,'/', n2,'-', n3,'/', n4,'=', n5)
+    elif n1/n2*n3+n4==n5:
+        print(n1,'/', n2,'*', n3,'+', n4,'=', n5)
+    elif n1/n2*n3-n4==n5:
+        print(n1,'/', n2,'*', n3,'-', n4,'=', n5)
+    elif n1/n2*n3*n4==n5:
+        print(n1,'/', n2,'*', n3,'*', n4,'=', n5)
+    elif n1/n2*n3/n4==n5:
+        print(n1,'/', n2,'*', n3,'/', n4,'=', n5)
+    elif n1/n2/n3+n4==n5:
+        print(n1,'/', n2,'/', n3,'+', n4,'=', n5)
+    elif n1/n2/n3-n4==n5:
+        print(n1,'/', n2,'/', n3,'-', n4,'=', n5)
+    elif n1/n2/n3*n4==n5:
+        print(n1,'/', n2,'/', n3,'*', n4,'=', n5)
+    elif n1/n2/n3/n4==n5:
+        print(n1,'/', n2,'/', n3,'/', n4,'=', n5)
+    else:
+        print('relation not satisfied.')
